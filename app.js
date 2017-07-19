add6fea434ef741668c8636a004e69d39078a17e
Yvette Menase <yvette.j.menase@gmail.com>
2017-07-19 12:39:44 -0600

    creating the posts page

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 angular.module('flapperNews', ['ui.router'])
-
 app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    });
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
 
-  $urlRouterProvider.otherwise('home');
-}]);
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+
+        $urlRouterProvider.otherwise('home');
+    }])
 
 .factory('posts', [function() {
         var o = {
-            posts: [{
-                title: 'hello',
-                link: '',
-                upvotes: 0
-            }]
+            posts: []
         };
         return o;
     }])
-
 
     .controller('MainCtrl', [
         '$scope',
@@ -40,9 +40,13 @@ function($stateProvider, $urlRouterProvider) {
                     return;
                 }
                 $scope.posts.push({
-                    title: $scope.title,
-                    link: $scope.link,
-                    upvotes: 0
+                  title: $scope.title,
+                  link: $scope.link,
+                  upvotes: 0,
+                  comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                  ]
                 });
                 $scope.title = '';
                 $scope.link = '';
@@ -51,6 +55,26 @@ function($stateProvider, $urlRouterProvider) {
             $scope.incrementUpvotes = function(post) {
                 post.upvotes += 1;
             };
-
         }
-    ]);
+    ])
+    app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+
+      $scope.post = posts.posts[$stateParams.id];
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
+
+
+
+
+    }]);
