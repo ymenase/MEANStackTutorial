4df7cbbfaaebc5189ff1a56cc5085237f526cfd5
Yvette Menase <yvette.j.menase@gmail.com>
2017-07-19 12:00:52 -0600

    added routing views

---
+++ app.js
@@ -0,0 +1,56 @@
+angular.module('flapperNews', ['ui.router'])
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+.factory('posts', [function() {
+        var o = {
+            posts: [{
+                title: 'hello',
+                link: '',
+                upvotes: 0
+            }]
+        };
+        return o;
+    }])
+
+
+    .controller('MainCtrl', [
+        '$scope',
+        'posts',
+        function($scope, posts) {
+            $scope.test = 'Hello world!';
+
+            $scope.posts = posts.posts;
+
+            $scope.addPost = function() {
+                if (!$scope.title === '') {
+                    return;
+                }
+                $scope.posts.push({
+                    title: $scope.title,
+                    link: $scope.link,
+                    upvotes: 0
+                });
+                $scope.title = '';
+                $scope.link = '';
+            };
+
+            $scope.incrementUpvotes = function(post) {
+                post.upvotes += 1;
+            };
+
+        }
+    ]);
